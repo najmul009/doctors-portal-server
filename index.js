11014c0f17636277dc41ed5204c71ca8fe185221
najmul009 <najmul.tk16@gmail.com>
2022-05-15 16:08:17 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -7,6 +7,28 @@ app.use(cors())
 app.use(express.json())
 require('dotenv').config();
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r5npa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const slotsCollection = client.db("doctors-db").collection("booking-slots")
+
+
+        app.get('/slots', async (req, res) => {
+            const query = {}
+            const cursor = slotsCollection.find(query)
+            const slots = await cursor.toArray()
+            res.send(slots)
+        })
+
+    }
+    finally {
+    }
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send('server started')
